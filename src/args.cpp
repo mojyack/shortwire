54f11884c5b680300ec265332af03940bd03a76c
mojyack <mojyack@gmail.com>
2024-08-10 18:57:17 +0900

    args: add short option name

--- src/args.cpp
+++ src/args.cpp
@@ -4,13 +4,13 @@
 auto Args::parse(const int argc, const char* const argv[]) -> std::optional<Args> {
     auto args   = Args();
     auto parser = args::Parser<uint8_t, uint16_t>();
-    parser.kwarg(&args.peer_linker_addr, {"--peer-linker-addr"}, {"HOSTNAME", "peer-linker address"});
-    parser.kwarg(&args.peer_linker_port, {"--peer-linker-port"}, {"PORT", "peer-linker port number"});
-    parser.kwarg(&args.peer_linker_user_cert_path, {"--peer-linker-cert"}, {"FILE", "user certificate for peer-linker", args::State::Initialized});
-    parser.kwarg(&args.key_file, {"--key"}, {"FILE", "enable encryption using the key", args::State::Initialized});
-    parser.kwarg(&args.subnet, {"--subnet"}, {"SUBNET", "x of 192.168.x.(1,2)", args::State::DefaultValue});
-    parser.kwarg(&args.server, {"--server"}, {"", "act as a server", args::State::Initialized});
-    parser.kwarg(&args.ws_only, {"--websocket-only"}, {"", "do not use p2p connection", args::State::Initialized});
+    parser.kwarg(&args.peer_linker_addr, {"-pa", "--peer-linker-addr"}, {"HOSTNAME", "peer-linker address"});
+    parser.kwarg(&args.peer_linker_port, {"-pp", "--peer-linker-port"}, {"PORT", "peer-linker port number"});
+    parser.kwarg(&args.peer_linker_user_cert_path, {"-pc", "--peer-linker-cert"}, {"FILE", "user certificate for peer-linker", args::State::Initialized});
+    parser.kwarg(&args.key_file, {"-k", "--key"}, {"FILE", "enable encryption using the key", args::State::Initialized});
+    parser.kwarg(&args.subnet, {"-n", "--subnet"}, {"SUBNET", "x of 192.168.x.(1,2)", args::State::DefaultValue});
+    parser.kwarg(&args.server, {"-s", "--server"}, {"", "act as a server", args::State::Initialized});
+    parser.kwarg(&args.ws_only, {"-wo", "--websocket-only"}, {"", "do not use p2p connection", args::State::Initialized});
     parser.kwarg(&args.ws_only, {"-v"}, {"", "enable verbose output", args::State::Initialized});
     parser.kwarg(&args.help, {"-h", "--help"}, {.arg_desc = "print this help message", .state = args::State::Initialized, .no_error_check = true});
     if(!parser.parse(argc, argv) || args.help) {
