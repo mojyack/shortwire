6cde8e1a36d89a739148f793942ca06281bbe674
mojyack <mojyack@gmail.com>
2024-08-10 19:03:23 +0900

    args: set default value for peer-linker-port

--- src/args.cpp
+++ src/args.cpp
@@ -5,7 +5,7 @@ auto Args::parse(const int argc, const char* const argv[]) -> std::optional<Args
     auto args   = Args();
     auto parser = args::Parser<uint8_t, uint16_t>();
     parser.kwarg(&args.peer_linker_addr, {"-pa", "--peer-linker-addr"}, {"HOSTNAME", "peer-linker address"});
-    parser.kwarg(&args.peer_linker_port, {"-pp", "--peer-linker-port"}, {"PORT", "peer-linker port number"});
+    parser.kwarg(&args.peer_linker_port, {"-pp", "--peer-linker-port"}, {"PORT", "peer-linker port number", args::State::DefaultValue});
     parser.kwarg(&args.peer_linker_user_cert_path, {"-pc", "--peer-linker-cert"}, {"FILE", "user certificate for peer-linker", args::State::Initialized});
     parser.kwarg(&args.key_file, {"-k", "--key"}, {"FILE", "enable encryption using the key", args::State::Initialized});
     parser.kwarg(&args.subnet, {"-n", "--subnet"}, {"SUBNET", "x of 192.168.x.(1,2)", args::State::DefaultValue});
--- src/args.hpp
+++ src/args.hpp
@@ -5,12 +5,12 @@ struct Args {
     const char* key_file                   = nullptr;
     const char* peer_linker_user_cert_path = nullptr;
     const char* peer_linker_addr;
-    uint16_t    peer_linker_port;
-    uint8_t     subnet  = 2;
-    bool        verbose = false;
-    bool        server  = false;
-    bool        ws_only = false;
-    bool        help    = false;
+    uint16_t    peer_linker_port = 8080;
+    uint8_t     subnet           = 2;
+    bool        verbose          = false;
+    bool        server           = false;
+    bool        ws_only          = false;
+    bool        help             = false;
 
     static auto parse(int argc, const char* const argv[]) -> std::optional<Args>;
 };
